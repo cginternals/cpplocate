b79574d12ff7ea688a31aa8c6181e907767cf392
Christopher <christopher.weyand@student.hpi.de>
2017-03-24 17:29:22 +0100

    Add tests

---
+++ source/tests/cpplocate-test/utils_test.cpp
@@ -0,0 +1,44 @@
+
+#include <gmock/gmock.h>
+
+#include <cpplocate/utils.h>
+
+
+using namespace cpplocate;
+
+
+class utils_test : public testing::Test
+{
+public:
+    utils_test()
+    {
+    }
+};
+
+
+TEST_F(utils_test, posAfterString_once)
+{
+    const std::string str = "posAfter378String";
+
+    auto val = utils::posAfterString(str, "378");
+
+    ASSERT_EQ(8, val);
+}
+
+TEST_F(utils_test, posAfterString_mult)
+{
+    const std::string str = "pos378After378String";
+
+    auto val = utils::posAfterString(str, "378");
+
+    ASSERT_EQ(14, val);
+}
+
+TEST_F(utils_test, posAfterString_not_found)
+{
+    const std::string str = "pos377After379String";
+
+    auto val = utils::posAfterString(str, "378");
+
+    ASSERT_EQ(std::string::npos, val);
+}
