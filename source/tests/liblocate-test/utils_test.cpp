252ab7838f6a0795d26818c1dd33f8b2c71e60f6
Willy Scheibel <willy.scheibel@hpi.de>
2018-04-01 01:33:55 +0200

    Start tests

--- source/tests/liblocate-test/utils_test.cpp
+++ source/tests/liblocate-test/utils_test.cpp
@@ -11,3 +11,31 @@ public:
     {
     }
 };
+
+
+TEST_F(utils_test, unifiedPath_NoPath)
+{
+    char * str1 = "A-string";
+
+    unifiedPath(str1, strlen(str1));
+
+    EXPECT_STREQ(str1, "A-string");
+}
+
+TEST_F(utils_test, unifiedPath_UnixPath)
+{
+    char * str1 = "/usr/include/c++/v1/tuple";
+
+    unifiedPath(str1, strlen(str1));
+
+    EXPECT_STREQ(str1, "/usr/include/c++/v1/tuple");
+}
+
+TEST_F(utils_test, unifiedPath_WindowsPath)
+{
+    char * str1 = "C:\\dev\\include\\c++\\v1\\tuple";
+
+    unifiedPath(str1, strlen(str1));
+
+    EXPECT_STREQ(str1, "C:/dev/include/c++/v1/tuple");
+}
