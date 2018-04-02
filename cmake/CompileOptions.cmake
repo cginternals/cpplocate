f71fe49195fe3cd6d9e438ed158caeaf5da3fa5b
Willy Scheibel <willy.scheibel@hpi.de>
2018-04-02 22:25:41 +0200

    Set c standard in cmake-init

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -22,6 +22,7 @@ endif()
 set(DEFAULT_PROJECT_OPTIONS
     DEBUG_POSTFIX             "d"
     CXX_STANDARD              11 # Not available before CMake 3.1; see below for manual command line argument addition
+    C_STANDARD                11
     LINKER_LANGUAGE           "CXX"
     POSITION_INDEPENDENT_CODE ON
     CXX_VISIBILITY_PRESET     "hidden"
